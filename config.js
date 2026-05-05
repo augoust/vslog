e16cb0fb8f07c3b8739db65827df7200caf80592
augoust <83108182+augoust@users.noreply.github.com>
2026-05-05 13:17:31 +0300

    Add visitor configuration to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.VISITOR_CONFIG = {
+  url:      'https://qvzgapexwyfyxlydzdra.supabase.co',
+  key:      'sb_publishable_cmA5v2oZ-j8CEbrsJduHNw_woLyRwjI',
+  table:    'visits',
+  location: 'Main Entrance',
+};
